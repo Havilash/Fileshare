60e701c01172570b35b1209b7a63a912f4c713f5
Havilash Sivaratnam <havilash2005@gmail.com>
2023-06-15 11:17:57 +0200

    update

--- fileshare/frontend/src/pages/Share/Share.jsx
+++ fileshare/frontend/src/pages/Share/Share.jsx
@@ -6,6 +6,8 @@ import { mimeTypeIcons } from "src/data/mimeTypes";
 import { getShare } from "src/lib/api";
 import Popup from "src/components/Popup/Popup";
 
+const colors = ["#FFB3BA", "#FFDFBA", "#FFFFBA", "#BAFFC9", "#BAE1FF"];
+
 export default function UpdateShare() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
@@ -40,18 +42,39 @@ export default function UpdateShare() {
   const handleEditShare = (e) => {};
 
   return (
-    <section className="share container">
-      <div className="share__container">
+    <section className="share-page container">
+      <div className="share-page__container">
         {showPopup && <Popup>Share Link copied to clipboard!</Popup>}
         <button className="button edit-share" onClick={handleEditShare}>
           Edit
         </button>
-        {share &&
-          share.files.map(
-            ({ name, content, updated_at, created_at, mime_type }, index) => (
-              <div></div>
-            )
-          )}
+        <div className="tiles">
+          {share &&
+            share.files.map(
+              ({ name, content, updated_at, created_at, mime_type }, index) => (
+                <div
+                  className="tile"
+                  key={index}
+                  style={{
+                    backgroundColor:
+                      colors[Math.floor(Math.random() * colors.length)],
+                  }}
+                >
+                  <div className="preview">
+                    {React.createElement(mimeTypeIcons[mime_type] || FiFile, {
+                      size: "3em",
+                    })}
+                  </div>
+                  <div className="details">
+                    <div className="details__name">{name}</div>
+                    <div className="details__date">
+                      {new Date(updated_at).toLocaleDateString()}
+                    </div>
+                  </div>
+                </div>
+              )
+            )}
+        </div>
       </div>
     </section>
   );
